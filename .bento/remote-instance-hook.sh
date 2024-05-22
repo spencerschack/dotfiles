22e2ce6ff08f2c611cf2309eba6d282182ef3a01
Spencer Schack <scs1818@gmail.com>
2024-05-22 10:45:23 -0700

    Sync: Wed May 22 10:38:40 PDT 2024

--- .bento/remote-instance-hook.sh
+++ .bento/remote-instance-hook.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd ~/carrot/customers/customers-backend
-sudo gem install rubocop-daemon --no-document
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
-# https://github.com/fohte/rubocop-daemon/tree/master?tab=readme-ov-file#more-speed
-curl https://raw.githubusercontent.com/fohte/rubocop-daemon/master/bin/rubocop-daemon-wrapper -o /tmp/rubocop-daemon-wrapper
-sudo mkdir -p /usr/local/bin/rubocop-daemon-wrapper
-sudo mv /tmp/rubocop-daemon-wrapper /usr/local/bin/rubocop-daemon-wrapper/rubocop
-sudo chmod +x /usr/local/bin/rubocop-daemon-wrapper/rubocop
+echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.zshrc.d/zsh-autosuggestions.zsh
