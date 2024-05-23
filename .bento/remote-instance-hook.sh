fdd8b6ea4d0b2e07b9336abadc12220a8167389d
Spencer Schack <scs1818@gmail.com>
2024-05-23 14:09:29 -0700

    Sync: Thu May 23 13:53:52 PDT 2024

--- .bento/remote-instance-hook.sh
+++ .bento/remote-instance-hook.sh
@@ -3,3 +3,11 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.zshrc.d/zsh-autosuggestions.zsh
+
+curl -L -o ~/.local/bin/open https://raw.githubusercontent.com/superbrothers/opener/master/bin/open
+
+alias open="~/.local/bin/open"
+
+echo "StreamLocalBindUnlink yes" | sudo tee /etc/ssh/sshd_config.d/opener.conf
+
+sudo systemctl restart ssh
